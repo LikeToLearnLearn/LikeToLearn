bef0e49f4349ce63dc7912385b3b8c60ddf8dae8
jenfor <jenfor@student-chalmers.se>
2016-01-20 16:05:15 +0100

    Add boatportal script, new island prototype

---
+++ Assets/boatportal.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class boatportal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void OnTriggerEnter(Collider c)
+    {
+        Debug.Log("collision det");
+        if (c.tag.Equals("Player"))
+        {
+
+            Debug.Log("collision det with player");
+            SceneManager.LoadScene("foundation");
+        }
+
+    }
+}
