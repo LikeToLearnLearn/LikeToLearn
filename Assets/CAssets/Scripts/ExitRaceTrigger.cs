987bba1992d47d59a37d31ca7668e60ed2d8de98
jenfor <jenfor@student-chalmers.se>
2016-01-29 15:07:44 +0100

    Car

---
+++ Assets/CAssets/Scripts/ExitRaceTrigger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ExitRaceTrigger : MonoBehaviour {
+
+    public GameObject car;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void OnTriggerEnter(Collider c)
+    {
+        car = GameObject.Find("Car");
+       Debug.Log("collision det");
+        //if (c.tag.Equals("PlayerCar"))
+        if (c.Equals(GameObject.Find("Car")))
+        {
+            car.SetActive(false);
+            player.SetActive(true);
+            //Debug.Log("collision det with player");
+            //SceneManager.LoadScene("game_racingisland");
+        }
+
+    }
+}
---
+++ Assets/CAssets/Scripts/StartRaceTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartRaceTrigger : MonoBehaviour {
+
+    public GameObject car;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void OnTriggerEnter(Collider c)
+    {
+       // Debug.Log("collision det");
+        if (c.tag.Equals("Player"))
+        {
+            car.SetActive(true);
+            player.SetActive(false);
+            //Debug.Log("collision det with player");
+            //SceneManager.LoadScene("game_racingisland");
+        }
+
+    }
+}
