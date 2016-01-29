6d1cb4ce4a91004a02e7ed63e29f9e632d468490
Johan Sjöberg <jsjoberg@gmail.com>
2016-01-29 14:29:12 +0100

    Added remove methods to inventory.

--- Assets/CAssets/Scripts/Inventory.cs
+++ Assets/CAssets/Scripts/Inventory.cs
@@ -6,7 +6,8 @@ using System.Collections.Generic;
 public class Inventory : MonoBehaviour {
     public enum Item { Fish, Brick, OneCoin, TenCoin, HundredBill, ThousandBill };
     private Dictionary<Item, int> acquiredItems;
-    public static Dictionary<Item, int> itemValue = new Dictionary<Item, int>() {
+
+	public static Dictionary<Item, int> itemValue = new Dictionary<Item, int>() {
         { Item.OneCoin, 1 },
         { Item.TenCoin, 10 },
         { Item.HundredBill, 100 },
@@ -28,6 +29,11 @@ public class Inventory : MonoBehaviour {
     void Start () {
         AddItem(Item.OneCoin);
         AddItems(Item.Fish, 30);
+		RemoveItems(Item.Fish, 24);
+		RemoveItem(Item.OneCoin);
+		RemoveItem(Item.TenCoin);
+		AddItem(Item.OneCoin);
+		AddItem(Item.OneCoin);
 	}
 	
 	// Update is called once per frame
@@ -54,19 +60,37 @@ public class Inventory : MonoBehaviour {
         }
     }
 
-    public void AddItems(Item i, int count)
+    public void AddItems(Item item, int count)
     {
         if (acquiredItems == null)
            acquiredItems = new Dictionary<Item, int>();
-        int old = acquiredItems.ContainsKey(i) ? acquiredItems[i] : 0;
-        acquiredItems[i] = old + count;
+        int old = acquiredItems.ContainsKey(item) ? acquiredItems[item] : 0;
+        acquiredItems[item] = old + count;
         redraw = true;
     }
 
-    public void AddItem(Item i)
+    public void AddItem(Item item)
     {
-        AddItems(i, 1);
+        AddItems(item, 1);
     }
+
+	public int RemoveItems(Item item, int count) 
+	{
+		if (acquiredItems == null || !acquiredItems.ContainsKey(item))
+			return 0;
+		int have = acquiredItems[item];
+		int take = count > have ? have : count;
+		acquiredItems[item] = have - take;
+		if (take > 0)
+			redraw = true;
+		return take;
+	}
+	
+
+	public int RemoveItem(Item item) 
+	{
+		return RemoveItems(item, 1);
+	}
 
 
     void OnGui()
