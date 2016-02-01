cd730b02fd0fdd7317d5db069a90e4e7d7db0955
jenfor <jenfor@student-chalmers.se>
2016-02-01 16:26:11 +0100

    Logic in RacingIsland

---
+++ Assets/CAssets/Scripts/RacingScrips/PickUpRightAnswere.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+
+public class PickUpRightAnswere : MonoBehaviour {
+    public GameObject text;
+    private RacingLogic racingLogic;
+
+    private float points;
+    private float f;
+
+    // Use this for initialization
+    void Start() {
+
+        GameObject racingLogicObject = GameObject.FindWithTag("RacingController");
+        if (racingLogicObject != null)
+        {
+            racingLogic = racingLogicObject.GetComponent<RacingLogic>();
+        }
+        if(racingLogic == null)
+        {
+            Debug.Log("Cannot find 'RacingLogic' script");
+        }
+
+        racingLogic.CreateMultiplication(4);
+        Debug.Log("Right answere :" + racingLogic.
+            GetMultiplicationAnswere());
+
+        points = 0;
+        SetValue();
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.CompareTag("Player"))
+        {
+
+            gameObject.SetActive(false);
+
+            racingLogic.AddScore(10);
+
+            points = points + f;
+            //Debug.Log(points);
+
+
+        }
+    }
+
+
+    void SetValue()
+    {
+        f = racingLogic.GetMultiplicationAnswere();
+        text.GetComponent<TextMesh>().text = "" +f;
+
+    }
+   
+
+   
+}
+  
---
+++ Assets/CAssets/Scripts/RacingScrips/RacingLogic.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class RacingLogic : MonoBehaviour
+{
+    private float points;
+    private float f;
+    private float answere;
+
+    public GUIText scoreText;
+    public GameObject text;
+
+    // Use this for initialization
+    void Start()
+    {
+       
+       points = 0;
+       //UpdateScore();
+       
+    }
+
+    void SetMultiplicationAnswere(float i)
+    {
+        answere = i;
+
+    }
+
+    public float GetMultiplicationAnswere()
+    {
+        return answere;
+
+    }
+
+    float SetValue(float i)
+    {
+
+        return Mathf.Floor(Random.value * i);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SetPoints(float f)
+    {
+        points = points + f;
+    }
+
+    public void AddScore(float newScoreValue)
+    {
+        points += newScoreValue;
+        UpdateScore();
+        Debug.Log("Present points: " + points);
+    }
+
+    void UpdateScore()
+    {
+        //scoreText.text = "Score: " + points;
+        text.GetComponent<TextMesh>().text = "Score: " + points;
+    }
+
+    public void CreateMultiplication(float n)
+    {
+        float a = n;
+        float b = SetValue(10);
+        SetMultiplicationAnswere(a * b);
+        text.GetComponent<TextMesh>().text = a + " * " + b;
+        
+    }
+
+   
+
+   
+}
+
+
+
+
