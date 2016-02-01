6df498c806b312cc8ce40b57b711d32d31a1f9a3
qualu <qualuo@gmail.com>
2016-02-01 22:11:37 +0100

    Use particle systems, implement math problems mechanic

---
+++ Assets/AnswerPoint.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnswerPoint : MonoBehaviour
+{
+
+    private float currentAnswer;
+    private bool triggered;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.tag.Equals("PlayerBoat"))
+        {
+            SetTriggered(true);
+        }
+    }
+
+    public void SetTriggered(bool b)
+    {
+        triggered = b;
+    }
+
+    public bool GetTriggered()
+    {
+        return triggered;
+    }
+
+    public void SetCurrentAnswer(float ans)
+    {
+        currentAnswer = ans;
+    }
+    public float GetCurrentAnswer()
+    {
+        return currentAnswer;
+    }
+}
---
+++ Assets/CAssets/Scripts/MathQGenerator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MathQGenerator : ScriptableObject {
+    
+    private enum operators
+    {
+        addition, subtraction, multiplication, division
+    }
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public static int[] GenerateIntMultiplicationQ(int operations, int min, int max)
+    {
+        int i = 0;
+        int[] question = new int[1+2*operations];
+        question[i] = Random.Range(min, max + 1);
+        
+        while ( i < question.Length - 1)
+        {
+            question[++i] = (int) operators.multiplication;
+            question[++i] = Random.Range(min, max + 1);
+        }
+
+        return question;
+    }
+}
