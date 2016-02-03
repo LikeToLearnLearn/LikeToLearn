6d6380b83bd90b87d54bd022dcad73a0cc9c9061
fific <fifi-johansson@hotmail.com>
2016-02-03 16:18:31 +0100

    keep working on fishing game

--- Assets/CAssets/Scripts/FishScript.cs
+++ Assets/CAssets/Scripts/FishScript.cs
@@ -2,23 +2,27 @@
 using System.Collections;
 
 public class FishScript : MonoBehaviour {
-    public Vector3 pointB;
+    public Vector3 pointB, pointC;
 
     // Use this for initialization
     IEnumerator Start()
     {
         var pointA = transform.position;
-        pointB = new Vector3(1.89f, -0.049f, -9.02f);
+        pointB = new Vector3(pointA.x + SetValue(5), pointA.y, pointA.z - SetValue(5));
+        pointC = new Vector3(pointB.x + SetValue(5), pointB.y - SetValue(2), pointB.z - SetValue(5));
         while (true)
         {
             yield return StartCoroutine(MoveObject(transform, pointA, pointB, 3.0f));
-            yield return StartCoroutine(MoveObject(transform, pointB, pointA, 3.0f));
+            yield return StartCoroutine(MoveObject(transform, pointB, pointC, 3.0f));
+            //turn fish
+            transform.Rotate(Vector3.right * Time.deltaTime);
+            yield return StartCoroutine(MoveObject(transform, pointC, pointA, 3.0f));
         }
     }
 
     // Update is called once per frame
     void Update () {
-        //Animation.Play();
+
     }
 
     IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
@@ -31,5 +35,10 @@ public class FishScript : MonoBehaviour {
             thisTransform.position = Vector3.Lerp(startPos, endPos, i);
             yield return null;
         }
+    }
+
+    float SetValue(float i)
+    {
+        return Mathf.Floor(Random.value * i);
     }
 }
