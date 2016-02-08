ecc91182c5ffa785989af54b683129ebd4e65b0f
qualu <qualuo@gmail.com>
2016-02-08 15:31:26 +0100

    Develop on placing items (building)

--- Assets/CAssets/Scripts/Building.cs
+++ Assets/CAssets/Scripts/Building.cs
@@ -5,55 +5,85 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Building : MonoBehaviour {
 
     public BuildGUIScript buildgui;
-    public Object brick;
-    public GameObject transparentbrick;
+    public GameObject currentItem;
+    public GameObject currentItemMarker;
+    public GameObject player;
+    private Camera camera;
+
+    private Ray ray;
+    private RaycastHit hit;
+
+    private bool snapMode = false;
 
     // Use this for initialization
     void Start () {
-
+        player = GameObject.FindGameObjectWithTag("Player");
+        camera = Camera.main;
         buildgui = GameObject.Find("BuildGUI").GetComponent<BuildGUIScript>();
-        //TODO Get item counts or current item properly!
 
-        Instantiate(transparentbrick, new Vector3(), Quaternion.identity); //TODO On chosen item, make a transparent version of it to show!
+        //TODO Get item counts and current item properly!
+        currentItem = GameObject.Find("Brick");
+        currentItemMarker = GameObject.Find("TransparentBrick");
+
+        Instantiate(currentItem, new Vector3(), Quaternion.identity); //TODO On chosen item, make a transparent version of it to show!
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        // Move transparent version of current chosen item to where camera is pointing
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        if(currentItem != null)
         {
-            transparentbrick.transform.position = hit.point;
-        }
-
-        // On Fire1 (ctrl/lmb/touch) place the real item where camera is pointing
-        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
-        {
-            RaycastHit hit2;
-            if (Physics.Raycast(ray, out hit2))
+            // Move transparent version of current chosen item to where camera is pointing
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out hit);
+            float dist = Vector3.Distance(hit.point, player.transform.position);
+            if (dist < 1)
             {
-                //if (hit.collider.name.Equals("Terrain"))
-                //{   }
-                Instantiate(brick, hit2.point, Quaternion.identity);
+                return;
+            }
+            else if (dist < 5 && !hit.collider.name.Contains("Clone"))
+            {
+                currentItemMarker.transform.position = hit.point;
+            }   
+            else
+            {
+                currentItemMarker.transform.position = ray.GetPoint(5);
+            }
+
+            currentItemMarker.transform.rotation = Quaternion.identity; 
+                                                //player.transform.rotation; to make item turn with player
+                                                //Quaternion.identity; to make item never turn, all placed items align
+
+            // On Fire1 (ctrl/lmb/touch) place the real item where camera is pointing
+            if (CrossPlatformInputManager.GetButtonDown("Fire1") && !snapMode)
+            {
+                //RaycastHit hit2;
+                //if (Physics.Raycast(ray, out hit2))
+                //{
+
+                Instantiate(currentItem, currentItemMarker.transform.position, currentItemMarker.transform.rotation);
 
 
+                //}
+
+            }
+            else if(CrossPlatformInputManager.GetButtonDown("Fire1") && snapMode)
+            {
+
+            }
+
+
+            if (CrossPlatformInputManager.GetButton("Fire2"))
+            {
+                //if (Physics.Raycast(ray, out hit))
+                //{
+                    Instantiate(currentItem, currentItemMarker.transform.position, currentItemMarker.transform.rotation);
+                //}
             }
 
         }
 
-        if (CrossPlatformInputManager.GetButton("Fire2"))
-        {
-            if (Physics.Raycast(ray, out hit))
-            {
-                //if (hit.collider.name.Equals("Terrain"))
-                //{   }
-                Instantiate(brick, hit.point, Quaternion.identity);
 
 
-            }
-        }
     }
 }
