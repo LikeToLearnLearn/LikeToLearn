c69f9f05c00071cc6e5167a40c93a330c6f1d671
qualu <qualuo@gmail.com>
2016-02-23 10:10:03 +0100

    Improve movement

--- Assets/FishController.cs
+++ Assets/FishController.cs
@@ -13,31 +13,34 @@ public class FishController : MonoBehaviour {
     void Start()
     {
         currentSpeed = 0f;
-        maxSpeed = 1000f;
+        maxSpeed = 500f;
         rb = GetComponent<Rigidbody>();
     }
 
 
     void FixedUpdate()
     {
-        Mathf.Lerp(currentSpeed, maxSpeed, 10);
+        currentSpeed = Mathf.Lerp(currentSpeed, maxSpeed, 0.4f);
         rb.AddRelativeForce(Vector3.forward * currentSpeed);
 
         Quaternion q = Quaternion.FromToRotation(transform.up, Vector3.up);
         float buoyantTorque = 100f;
         rb.AddTorque(q.x * 1 * buoyantTorque,0,q.y*buoyantTorque);
+
+        Quaternion deltaRotation = Quaternion.Euler(new Vector3(rb.rotation.x + Random.Range(-4,4), rb.rotation.y + Random.Range(-4, 4), rb.rotation.z + Random.Range(-4, 4) * Time.fixedDeltaTime));
+        rb.MoveRotation(rb.rotation * deltaRotation);
     }
 
     void OnCollisionEnter()
     {
-        currentSpeed = 200;
+        currentSpeed = 20;
     }
 
 
     void OnCollisionStay(Collision collisionInfo)
     {
         //print(collisionInfo.collider.gameObject.name);
-        Quaternion deltaRotation = Quaternion.Euler(new Vector3(rb.rotation.x + Random.Range(5, 15), rb.rotation.y + 30f, rb.rotation.z + Random.Range(5, 15)) * 5 *Time.fixedDeltaTime);
+        Quaternion deltaRotation = Quaternion.Euler(new Vector3(rb.rotation.x + Random.Range(-15, 15), rb.rotation.y + 150f, rb.rotation.z + Random.Range(-15, 15)) * 5 *Time.fixedDeltaTime);
         rb.MoveRotation(rb.rotation * deltaRotation);
     }
 }
