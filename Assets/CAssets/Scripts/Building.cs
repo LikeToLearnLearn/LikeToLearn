d8c66ea100454e1cc91fa0c56147240bef8a8ee9
qualu <qualuo@gmail.com>
2016-02-26 20:40:39 +0100

    Change building a lot. Blocks now snap! Bug fixes

--- Assets/CAssets/Scripts/Building.cs
+++ Assets/CAssets/Scripts/Building.cs
@@ -5,9 +5,11 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Building : MonoBehaviour {
 
     public BuildGUIScript buildgui;
+    public Material transparentMat;
 
     public GameObject currentItem;
     public GameObject currentItemMarker;
+    private float maxMarkerDistance;
     private Bounds currentItemBounds;
 
 	private string currentItemString;
@@ -27,11 +29,27 @@ public class Building : MonoBehaviour {
     private Vector3 snapCurrentPosition;
     private GameObject[] snapItemMarkers;
 
+    public bool add99999ofeveryitem = true;
+
     // Use this for initialization
-    void Start () {
+    void Start() {
+        maxMarkerDistance = 4;
+
         player = GameObject.FindGameObjectWithTag("Player");
         camera = GameObject.FindGameObjectWithTag("MainCamera");
         buildgui = GameObject.Find("BuildGUI").GetComponent<BuildGUIScript>();
+
+        //ITEMS CHEAT FOR TESTING!
+        if (add99999ofeveryitem)
+        {
+            string[] names = System.Enum.GetNames(typeof(GameController.Item));
+            GameController.Item[] items = (GameController.Item[])System.Enum.GetValues(typeof(GameController.Item));
+            foreach (GameController.Item i in items)
+            {
+                GameController.control.AddItems(i, 99999);
+            }
+        }
+        //ITEMS CHEAT!
     }
 
     // Update is called once per frame
@@ -42,16 +60,21 @@ public class Building : MonoBehaviour {
 			currentItemString = buildgui.chosenItem;
 			currentItem = (GameObject) Instantiate(Resources.Load("Build" + currentItemString));
 			currentItemBounds = currentItem.GetComponent<Renderer>().bounds;
+
+            if (currentItemMarker != null) Destroy(currentItemMarker);
 			currentItemMarker = (GameObject) Instantiate(currentItem, new Vector3(), Quaternion.identity);
 			currentItemMarker.name = "Transparent" + currentItemString;
+            currentItemMarker.GetComponent<Renderer>().material = transparentMat;
+            currentItemMarker.GetComponent<Collider>().enabled = false;
 		}
 
         if(currentItem != null)
         {
-            //if (currentMousePosition != Input.mousePosition)
-            //{
-            MoveCurrentItemMarker();
-                //}
+            if (currentMousePosition != Input.mousePosition)
+            {
+                markerSnapped = false;
+                MoveCurrentItemMarker();
+            }
 
                 // On Fire1 (ctrl/lmb) place an item where camera is pointing, initiate snapmode
                 if (CrossPlatformInputManager.GetButtonDown("Fire1") && !snapMode)
@@ -59,9 +82,9 @@ public class Building : MonoBehaviour {
     	            print("fire1 no snap");
 					if (GameController.control.RemoveItem(currentItemString) > 0) {
                     	Instantiate(currentItem, currentItemMarker.transform.position, currentItemMarker.transform.rotation);
-					}
-                    snapStartPosition = currentItemMarker.transform.position;
-                    snapMode = true;
+                        snapStartPosition = currentItemMarker.transform.position;
+                        snapMode = true;
+                    }
 
                 }
                 // While mouse is held down, move the marker around
@@ -78,50 +101,7 @@ public class Building : MonoBehaviour {
                 {
                     snapCurrentPosition = currentItemMarker.transform.position;
 
-                    // Calculate length of box in every dimension
-                    float distanceX = snapCurrentPosition.x - snapStartPosition.x;
-                    float distanceY = snapCurrentPosition.y - snapStartPosition.y;
-                    float distanceZ = snapCurrentPosition.z - snapStartPosition.z;
-
-                    // Determine directions box is to be offset toward
-                    int dirX = distanceX >= 0 ? 1 : -1;
-                    int dirY = distanceY >= 0 ? 1 : -1;
-                    int dirZ = distanceZ >= 0 ? 1 : -1;
-
-                    // Attempt at removing smallest of the 3 to only form a plane instead
-                    /*ArrayList list = new ArrayList();
-                    list.Add(distanceX);
-                    list.Add(distanceY);
-                    list.Add(distanceZ);
-                    list.Remove(Mathf.Min(distanceX,distanceY,distanceZ));
-                    list.Sort();*/
-
-                    // Calculate how many blocks fit in every dimension, with a max for performance reasons
-                    int blocksX = Mathf.Min(40, Mathf.CeilToInt(Mathf.Abs(distanceX) / currentItemBounds.size.x));
-                    int blocksY = Mathf.Min(40, Mathf.CeilToInt(Mathf.Abs(distanceY) / currentItemBounds.size.y));
-                    int blocksZ = Mathf.Min(40, Mathf.CeilToInt(Mathf.Abs(distanceZ) / currentItemBounds.size.z));
-
-                    // Place all blocks in a hollow box form
-                    for (int i = 0; i < blocksX; i++)
-                    {
-                        for (int j = 0; j < blocksY; j++)
-                        {
-                            for (int k = 0; k < blocksZ; k++)
-                            {
-                                if(i == 0 || j == 0 || k == 0 || 
-                                   i == blocksX-1|| j == blocksY-1 || k == blocksZ-1)
-                            {
-								if (GameController.control.RemoveItem(currentItemString) > 0) {
-	                                 latestPlaced = (GameObject) Instantiate(currentItem, snapStartPosition + new Vector3(
-	                                    dirX * i * currentItemBounds.size.x,
-	                                    dirY * j * currentItemBounds.size.y,
-	                                    dirZ * k * currentItemBounds.size.z),
-	                                    currentItemMarker.transform.rotation);
-								}
-                            }
-                            }
-                        }
-                    }
+                    StartCoroutine(PlaceItemsAsHollowBox());
 
                     // Place X-length blocks
                     /*if (distanceX < 0)
@@ -183,11 +163,10 @@ public class Building : MonoBehaviour {
                 }
 
 
-                // Mobile delete block on touch. UNTESTED
+                // Mobile delete block on touch. TO FIX: DELETES WHEN ANY TOUCH IS REGISTERED
                 if(Input.touches.Length != 0)
                 {
-                    Touch t = Input.GetTouch(0);
-                    Ray touchRay = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                    Ray touchRay = Camera.main.ScreenPointToRay(Input.mousePosition);
                     RaycastHit touchHit;
                     if (Physics.Raycast(touchRay, out touchHit))
                     {
@@ -196,20 +175,24 @@ public class Building : MonoBehaviour {
                             if (hit.collider.name.Contains("Clone"))
                             {
                                 Destroy(hit.collider.gameObject);
+                                snapMode = false;
                             }
                         }
                     }
                 }
                 
           
-            // Second button to remove a block. NO SECOND BUTTON ON MOBILE
+            // Second button to remove a block. This button doens't exist on mobile.
             if (CrossPlatformInputManager.GetButtonDown("Fire2"))
                 {
                     if (hit.collider != null)
                     {
                         if (hit.collider.name.Contains("Clone"))
                         {
+
+                            Physics.Raycast(ray, out hit);
                             Destroy(hit.collider.gameObject);
+                            snapMode = false;
                         }
                     }
                 }
@@ -223,14 +206,49 @@ public class Building : MonoBehaviour {
 
     private IEnumerator PlaceItemsAsHollowBox()
     {
+        // Calculate length of box in every dimension
+        float distanceX = snapCurrentPosition.x - snapStartPosition.x;
+        float distanceY = snapCurrentPosition.y - snapStartPosition.y;
+        float distanceZ = snapCurrentPosition.z - snapStartPosition.z;
 
+        // Determine directions box is to be offset toward
+        int dirX = distanceX >= 0 ? 1 : -1;
+        int dirY = distanceY >= 0 ? 1 : -1;
+        int dirZ = distanceZ >= 0 ? 1 : -1;
+
+        // Calculate how many blocks fit in every dimension, with a max for performance reasons
+        int blocksX = Mathf.Min(40, 1 + Mathf.FloorToInt(Mathf.Abs(distanceX) / currentItemBounds.size.x));
+        int blocksY = Mathf.Min(40, 1 + Mathf.FloorToInt(Mathf.Abs(distanceY) / currentItemBounds.size.y));
+        int blocksZ = Mathf.Min(40, 1 + Mathf.FloorToInt(Mathf.Abs(distanceZ) / currentItemBounds.size.z));
+
+        // Place all blocks in a hollow box form
+        for (int i = 0; i < blocksX; i++)
+        {
+            for (int j = 0; j < blocksY; j++)
+            {
+                for (int k = 0; k < blocksZ; k++)
+                {
+                    if (i == 0 || j == 0 || k == 0 ||
+                       i == blocksX - 1 || j == blocksY - 1 || k == blocksZ - 1)
+                    {
+                        if (GameController.control.RemoveItem(currentItemString) > 0)
+                        {
+                            latestPlaced = (GameObject)Instantiate(currentItem, snapStartPosition + new Vector3(
+                               dirX * i * currentItemBounds.size.x,
+                               dirY * j * currentItemBounds.size.y,
+                               dirZ * k * currentItemBounds.size.z),
+                               currentItemMarker.transform.rotation);
+                        }
+                    }
+                }
+            }
+        }
         yield return new WaitForSeconds(0);
     }
 
     private void MoveCurrentItemMarker()
     {
         // Move transparent version of current chosen item to where camera is pointing
-        print("move");
         ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2, 0));
         Physics.Raycast(ray, out hit);
         float dist = Vector3.Distance(hit.point, player.transform.position);
@@ -238,42 +256,20 @@ public class Building : MonoBehaviour {
         {
             return;
         }
-        else if (dist < 4)
+        else if (dist < maxMarkerDistance)
         {
-            if (hit.collider != null && !hit.collider.gameObject.name.Equals("Terrain")) // TODO fix this check to prevent exception
+            if (hit.collider != null && !hit.collider.gameObject.name.Equals("Terrain") && !markerSnapped)
             {
-                // WIP MAKE IT ACTUALLY SNAP NEXT TO BLOCK
-                if (hit.normal.x != 0 && !markerSnapped)
-                {
-                    float add = hit.collider.gameObject.GetComponent<Renderer>().bounds.extents.x;
-                    Vector3 a = new Vector3(add, 0, 0);
-                    currentItemMarker.transform.position = hit.collider.bounds.center + a;
-                    markerSnapped = true;
-                }
-                if (hit.normal.y != 0 && !markerSnapped)
-                {
-                    float add = hit.collider.gameObject.GetComponent<Renderer>().bounds.extents.y;
-                    Vector3 a = new Vector3(0, add, 0);
-                    currentItemMarker.transform.position = hit.collider.bounds.center + a;
-                    markerSnapped = true;
-                }
-                if (hit.normal.z != 0 && !markerSnapped)
-                {
-                    float add = hit.collider.gameObject.GetComponent<Renderer>().bounds.extents.z;
-                    Vector3 a = new Vector3(0, 0, add);
-                    currentItemMarker.transform.position = hit.collider.bounds.center + a;
-                    markerSnapped = true;
-                }
+                currentItemMarker.transform.position = hit.collider.bounds.center + Vector3.Scale(hit.collider.gameObject.GetComponent<Renderer>().bounds.size, hit.normal);
+                // Much smurt.
             }
-
-            //currentItemMarker.transform.position = hit.point;
         }
         else
         {
-            currentItemMarker.transform.position = ray.GetPoint(4);
+            currentItemMarker.transform.position = ray.GetPoint(maxMarkerDistance);
         }
 
-        currentItemMarker.transform.rotation = Quaternion.identity;
+        currentItemMarker.transform.rotation = player.transform.rotation; //Quaternion.identity;
         //player.transform.rotation; to make item turn with player
         //Quaternion.identity; to make item never turn, all placed items align (not to grid)
 
