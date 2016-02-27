00054277be0764cde6621158dc26ac30507b515d
qualu <qualuo@gmail.com>
2016-02-27 14:13:10 +0100

    Tweak building, add cloud system prefab, suggest new player

---
+++ Assets/SceneBorderScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneBorderScript : MonoBehaviour {
+
+    void OnCollisionEnter(Collision c)
+    {
+        if(c.collider.tag.Contains("Player"))
+        {
+            c.transform.position = GameObject.Find("SpawnPoint").transform.position;
+        }
+    }
+}
