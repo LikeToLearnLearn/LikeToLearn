afe08fc0fafb26a9aca6df446fe3682e179d4ae5
qualu <qualuo@gmail.com>
2016-02-27 15:08:03 +0100

    Tweak slower less erratic fish, tweak fish text, move scripts

--- Assets/CAssets/Scripts/FishingScripts/FishController.cs
+++ Assets/CAssets/Scripts/FishingScripts/FishController.cs
@@ -12,21 +12,23 @@ public class FishController : MonoBehaviour {
 
     void Start()
     {
-        currentSpeed = 0f;
-        maxSpeed = 300f;
+        currentSpeed = 50f;
+        maxSpeed = 100f;
         rb = GetComponent<Rigidbody>();
     }
 
 
     void FixedUpdate()
     {
-
+        if(Random.Range(0,200) == 0)
+        {
+            currentSpeed = 0;
+        }
         currentSpeed = Mathf.Lerp(currentSpeed, maxSpeed, 0.1f);
         rb.AddRelativeForce(Vector3.forward * currentSpeed);
 
         Quaternion q = Quaternion.FromToRotation(transform.up, Vector3.up);
-        float buoyantTorque = 100f;
-        rb.AddTorque(q.x * 1 * buoyantTorque, 0, q.y * buoyantTorque);
+        rb.AddTorque(q.x * 100, q.y * 100, q.z * 100);
 
         Quaternion deltaRotation = Quaternion.Euler(new Vector3(rb.rotation.x + Random.Range(-1, 1), rb.rotation.y + Random.Range(-1, 1), rb.rotation.z + Random.Range(-1, 1) * Time.fixedDeltaTime));
         rb.MoveRotation(rb.rotation * deltaRotation);
@@ -42,7 +44,7 @@ public class FishController : MonoBehaviour {
     void OnCollisionStay(Collision collisionInfo)
     {
         //print(collisionInfo.collider.gameObject.name);
-        Quaternion deltaRotation = Quaternion.Euler(new Vector3(rb.rotation.x + Random.Range(-15, 15), rb.rotation.y + 100f, rb.rotation.z + Random.Range(-15, 15)) * 5 *Time.fixedDeltaTime);
+        Quaternion deltaRotation = Quaternion.Euler(new Vector3(rb.rotation.x + Random.Range(-15, 15), rb.rotation.y + 150f * Random.Range(-1,1), rb.rotation.z + 70f * Random.Range(-1, 1)) * 5 *Time.fixedDeltaTime);
         rb.MoveRotation(rb.rotation * deltaRotation);
     }
 }
---
+++ Assets/RandomMovementScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Expects object to be contained in a shape of colliders to limit movement
+public class RandomMovementScript : MonoBehaviour {
+
+    private Rigidbody rb;
+    private float currentSpeed;
+    private float maxSpeed;
+
+
+    void Start()
+    {
+        currentSpeed = 0f;
+        maxSpeed = 100f;
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if(Random.Range(0,200) == 0)
+        {
+            currentSpeed = 0;
+        }
+        currentSpeed = Mathf.Lerp(currentSpeed, maxSpeed, 0.1f);
+        rb.AddRelativeForce(Vector3.forward * currentSpeed);
+
+        Quaternion q = Quaternion.FromToRotation(transform.up, Vector3.up);
+        float buoyantTorque = 100f;
+        rb.AddTorque(q.x * 1 * buoyantTorque, 0, q.y * buoyantTorque);
+
+        Quaternion deltaRotation = Quaternion.Euler(new Vector3(rb.rotation.x + Random.Range(-1, 1), rb.rotation.y + Random.Range(-1, 1), rb.rotation.z + Random.Range(-1, 1) * Time.fixedDeltaTime));
+        rb.MoveRotation(rb.rotation * deltaRotation);
+
+    }
+
+    void OnCollisionEnter()
+    {
+        currentSpeed = maxSpeed;
+    }
+
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        //print(collisionInfo.collider.gameObject.name);
+        Quaternion deltaRotation = Quaternion.Euler(new Vector3(rb.rotation.x + Random.Range(-15, 15), rb.rotation.y + 150f, rb.rotation.z + 70f) * 5 *Time.fixedDeltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+    }
+}
