dd61c5fe8e4d34cac0d3c072dfe44e96e50cfa8e
jenfor <jenfor@student-chalmers.se>
2016-03-30 10:38:31 +0200

    Test

---
+++ Assets/CAssets/Scripts/online/testConnection.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testConnection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        //string url = String.Format("https://ece01.ericsson.net:4443/ecity?dgw=Ericsson${0}&sensorSpec=Ericsson${1}&t1={2}&t2={3}", bus, sensor, t1, t2);
+        //var www = new WWW(url, null, headers);
+        // StartCoroutine(WaitForRequest(action, www));
+
+        //string productlist = File.ReadAllText(Application.dataPath + "/Resources/AssetBundles/" + "AssetBundleInfo.json");
+        //UploadProductListJSON(productList);
+
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+   
+    void ConnectToServer()
+    {
+        Network.Connect("192.168.254.169", 8080);
+    }
+
+    static void UploadJSON(string data)
+    {
+        Debug.Log(data);
+        WWWForm form = new WWWForm();
+
+        form.AddField("name", data);
+        //WWW www = new WWW("192.168.254.157:8080/greeting?name=Je", form);
+        WWW www = new WWW("192.168.254.157:8080/greeting?name=" + data);
+    }
+    
+}
+
+
+
+
+
